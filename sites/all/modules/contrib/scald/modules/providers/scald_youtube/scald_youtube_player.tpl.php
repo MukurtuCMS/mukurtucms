c4c3ffc06cb842245e39ee6d2ea9365379553da0
Shiraz Dindar <shiraz@extracelestial.com>
2015-06-08 09:46:47 -0700

    Update scald youtube due to youtube API changes. Scald_youtube is now a separate module. Requires deleting the old one, and a registry rebuild.

--- sites/all/modules/contrib/scald/modules/providers/scald_youtube/scald_youtube_player.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @file
- *   Default theme implementation for the Scald Youtube Player
- */
-?>
-<iframe width="<?php print $vars['video_width'] ?>" height="<?php print $vars['video_height'] ?>" src="//www.youtube.com/embed/<?php print $vars['video_id'] ?>" frameborder="0" allowfullscreen></iframe>
