53b999b95c0cafdbc49c3edf27ff1d9520982f04
Shiraz Dindar <shiraz@extracelestial.com>
2015-11-06 18:24:38 -0800

    Use a separate cookie (and the original JS) for the Collections browse mode since there is no map option there. Otherwise if a user selects Map mode in DH and then visits the Collections page the browse mode is unselected.

--- sites/all/modules/custom/features/ma_digitalheritage/js/dh_browse_mode_switcher.js
+++ sites/all/modules/custom/features/ma_digitalheritage/js/dh_browse_mode_switcher.js
@@ -17,9 +17,9 @@ Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
 
     // Sets default class for DH views and toggles between list/grid/map.
-    $('#main-content').once('dh-browse-view', function() {
+    $('#main-content').once('browse-view', function() {
 
-      if ($(".view").hasClass("dh-browse-view")) {
+      if ($(".view").hasClass("browse-view")) {
 
         $('a.list').addClass('active');
 
@@ -55,7 +55,7 @@ Drupal.behaviors.my_custom_behavior = {
 
           $.cookie('dh_browse_mode', 'grid-view', {path: "/"}); // set the cookie to grid mode
 
-          $('.view-digital-heritage-grid-list .view-content, .view-collections-grid-list .view-content').masonry();
+          $('.view-digital-heritage-grid-list .view-content').masonry();
 
           if (originalViewMode == "map-view") {
             location.reload();
@@ -72,7 +72,7 @@ Drupal.behaviors.my_custom_behavior = {
           $(".view").removeClass("grid-view"); // removes .grid-view from the view class
           $.cookie('dh_browse_mode', 'list-view', {path: "/"}); // set the cookie to list-view
 
-          $('.view-digital-heritage-grid-list .view-content, .view-collections-grid-list .view-content').masonry('destroy');
+          $('.view-digital-heritage-grid-list .view-content').masonry('destroy');
 
           if (originalViewMode == "map-view") {
             location.reload();
