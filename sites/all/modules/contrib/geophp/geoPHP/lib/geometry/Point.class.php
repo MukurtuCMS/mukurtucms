29bc07dfce97da6f8620843fea7985a1c90fd4d7
Shiraz Dindar <shiraz@kanopistudios.com>
2017-03-06 16:00:59 -0800

    Patch to geoPHP (the library therein) to avoid fatal error which is following notice condition of Undefined offset: 1 in WKT->parsePoint() (line 71 of /www/kivalina/sites/all/modules/contrib/geophp/geoPHP/lib/adapters/WKT.class.php)..

--- sites/all/modules/contrib/geophp/geoPHP/lib/geometry/Point.class.php
+++ sites/all/modules/contrib/geophp/geoPHP/lib/geometry/Point.class.php
@@ -26,6 +26,8 @@ class Point extends Geometry
       return;
     }
 
+    $x = floatval(str_replace(',', '.', $x));
+    $y = floatval(str_replace(',', '.', $y));
     // Basic validation on x and y
     if (!is_numeric($x) || !is_numeric($y)) {
       throw new Exception("Cannot construct Point. x and y should be numeric");
