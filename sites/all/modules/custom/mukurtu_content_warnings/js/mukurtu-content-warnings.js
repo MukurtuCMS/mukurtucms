2e6afa660c1ce1301c6abd70116624357244f608
Steve Taylor <steve_taylor@wsu.edu>
2017-07-11 16:05:11 -0700

    Change from IDs to classes for warnings
    The same scald atom can be displayed multiple time per page, causing
    duplicate IDs.

--- sites/all/modules/custom/mukurtu_content_warnings/js/mukurtu-content-warnings.js
+++ sites/all/modules/custom/mukurtu_content_warnings/js/mukurtu-content-warnings.js
@@ -1,5 +1,4 @@
 function mukurtu_remove_content_warning(id) {
-    jQuery("#mukurtu-content-warning-" + id).removeClass("mukurtu-content-warning");
-    jQuery("#mukurtu-content-warning-instance-" + id).addClass("cleared");
+    jQuery(".mukurtu-content-warning-" + id).removeClass("mukurtu-content-warning");
+    jQuery(".mukurtu-content-warning-instance-" + id).addClass("cleared");
 }
-
