bead0429a5facc5e104530238e8c82ba2f615a12
Steve Taylor <steve_taylor@wsu.edu>
2017-07-12 09:30:27 -0700

    Clean up unused code

--- sites/all/modules/custom/mukurtu_taxonomy_records/js/mukurtu_records.js
+++ sites/all/modules/custom/mukurtu_taxonomy_records/js/mukurtu_records.js
@@ -1,31 +1,5 @@
 (function ($) {
-    /*
-    // Sync checkboxes -> chosen
-    $(document).on("click", "#mukurtu-taxonomy-records-manage-record-form .form-item-terms .form-checkbox", function() {
-	var box = $(this);
-	var select = $("#edit-terms-all");
-
-	// User clicked checkbox. Sync that value with the select control for all terms
-	$("#edit-terms-all option[value='" + box.val() + "']").prop('selected', box.is(':checked'));
-
-	// Update chosen
-	$("#edit-terms-all").trigger("chosen:updated");
-    });
-
-    // Sync chosen -> checkboxes
-    $(document).on("change", "select#edit-terms-all", function(event, params) {
-	if(params.deselected) {
-	    $("#edit-terms input:checkbox[value='" + params.deselected + "']").prop('checked', false);
-	}
-
-	if(params.selected) {
-	    $("#edit-terms input:checkbox[value='" + params.selected + "']").prop('checked', true);
-	}
-    });
-*/
-
-
-    // Custom Widget
+    // Mukurtu Records Record Management Custom Widget
     $(document).on("click", "#edit-field-mukurtu-terms-und-mukurtu-record-terms .form-checkbox", function() {
 	var box = $(this);
 	var select = $("[id^=edit-field-mukurtu-terms-und-mukurtu-record-terms-all]");
