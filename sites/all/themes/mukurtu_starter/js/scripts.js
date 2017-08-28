0084d7bdc6899375427ef608cd560ec59b569995
Steve Taylor <steve_taylor@wsu.edu>
2017-08-28 15:50:38 -0700

    Fix tooltips on ckedtor fields

--- sites/all/themes/mukurtu_starter/js/scripts.js
+++ sites/all/themes/mukurtu_starter/js/scripts.js
@@ -19,5 +19,13 @@ jQuery(document).ready(function($){
         $('.view-digital-heritage-grid-list .view-content, .view-collections-grid-list .view-content').masonry('destroy');
     }
 
-
+    // ckedtor tooltip fix
+    // The ckeditor hides the original field textarea, which is where the tooltip is attached.
+    // Copy the tooltip to the parent wrapper.
+    var options = Drupal.settings.bootstrap.tooltipOptions;
+    $('.ckeditor-mod[data-toggle="tooltip"]').each(function() {
+	jQuery(this).parent().attr('data-toggle', 'ckeditor-tooltip');
+	jQuery(this).parent().attr('title', jQuery(this).attr('data-original-title'));
+    });
+    $('[data-toggle="ckeditor-tooltip"]').tooltip(options);
 });
