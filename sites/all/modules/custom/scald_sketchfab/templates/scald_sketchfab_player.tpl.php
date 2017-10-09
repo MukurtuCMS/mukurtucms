646b84dabb6eaddc8c88bcbc09d197def0c53185
Shiraz Dindar <shiraz@kanopistudios.com>
2017-10-09 15:46:21 -0700

    The heart of the custom scald provider for Sketchfab.

---
+++ sites/all/modules/custom/scald_sketchfab/templates/scald_sketchfab_player.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for the Scald Youtube Player.
+ */
+?>
+<div class="sketchfab-embed-wrapper"><iframe width="100%" height="480" src="<?php print $vars['video_url'] ?>/embed" frameborder="0" allowvr allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
