d22bc1be5dc41f287cc3982b39985fdad002648c
Shiraz Dindar <shiraz@kanopistudios.com>
2017-11-08 22:18:48 -0800

    Metadata on the sketchfab models. Title, description, tags, authors, and thumbnail fields. These are all prefilled on the upload form by grabbing the metadata from the sketchfab model via the sketchfab API (simple http get and json decode, no auth required). The metadata displays in a metadata popup when viewing the sketchfab model in colorbox display mode (eg, within a DH item), the same as the other atom types. Thsi commit also includes various bug fixes to sketchfab.

--- sites/all/modules/custom/scald_sketchfab/templates/scald_sketchfab_player.tpl.php
+++ sites/all/modules/custom/scald_sketchfab/templates/scald_sketchfab_player.tpl.php
@@ -4,4 +4,4 @@
  * Default theme implementation for the Scald Youtube Player.
  */
 ?>
-<div class="sketchfab-embed-wrapper"><iframe width="100%" height="480" src="<?php print $vars['video_url'] ?>/embed" frameborder="0" allowvr allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
+<div class="sketchfab-embed-wrapper"><iframe width="100%" height="480" src="https://sketchfab.com/models/<?php print $vars['sketchfab_id'] ?>/embed" frameborder="0" allowvr allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
