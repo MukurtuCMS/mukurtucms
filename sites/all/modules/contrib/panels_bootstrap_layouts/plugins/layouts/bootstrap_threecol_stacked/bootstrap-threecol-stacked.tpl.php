c7fd3ef5450bb422cc833221baab6856dcb70a05
Steve Taylor <steve_taylor@wsu.edu>
2018-03-30 09:59:30 -0700

    Switch to dev version of panels bootstrap layouts

--- sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_threecol_stacked/bootstrap-threecol-stacked.tpl.php
+++ sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_threecol_stacked/bootstrap-threecol-stacked.tpl.php
@@ -6,7 +6,7 @@
   <?php endif ?>
 
   <?php if ($content['left'] || $content['middle'] || $content['right']): ?>
-    <div class="row"> <!-- @TODO: Add extra classes -->
+    <div class="row">
       <?php print $content['left']; ?>
       <?php print $content['middle']; ?>
       <?php print $content['right']; ?>
--- sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_stacked/bootstrap-twocol-stacked.tpl.php
+++ sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_stacked/bootstrap-twocol-stacked.tpl.php
@@ -1,12 +1,20 @@
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="row">
-    <?php print $content['top']; ?>
-  </div>
-  <div class="row">
-    <?php print $content['left']; ?>
-    <?php print $content['right']; ?>
-  </div>
-  <div class="row">
-    <?php print $content['bottom']; ?>
-  </div>
+  <?php if ($content['top']): ?>
+    <div class="row">
+      <?php print $content['top']; ?>
+    </div>
+  <?php endif ?>
+
+  <?php if ($content['left'] || $content['right']): ?>
+    <div class="row">
+      <?php print $content['left']; ?>
+      <?php print $content['right']; ?>
+    </div>
+  <?php endif ?>
+
+  <?php if ($content['bottom']): ?>
+    <div class="row">
+      <?php print $content['bottom']; ?>
+    </div>
+  <?php endif ?>
 </div>
