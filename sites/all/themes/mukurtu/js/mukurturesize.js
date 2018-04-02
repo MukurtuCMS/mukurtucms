b77281c77dd28c5fc0480ea76a990af4ba6aa4b2
Steve Taylor <steve_taylor@wsu.edu>
2018-04-02 10:54:50 -0700

    Digital Heritage page mostly working

---
+++ sites/all/themes/mukurtu/js/mukurturesize.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($){
+    function refereshPanelAccordion() {
+	var width = $(window).width();
+	if(width < 768) {
+	    $(".pane-content.collapse").each( function () {
+		$(this).collapse("hide");
+	    });
+	} else {
+	    $(".pane-content.collapse").each( function () {
+		$(this).collapse("show");
+	    });
+	}
+    }
+
+    function mukurtuOnResize() {
+	refereshPanelAccordion();
+    }
+    window.onresize = mukurtuOnResize;
+    mukurtuOnResize();
+
+    $(window).load(function() {
+	mukurtuOnResize();
+    });
+});
