0dce31c6685e1c0ddd91e31949e7bc685aba8b38
Steve Taylor <steve_taylor@wsu.edu>
2018-04-20 15:28:27 -0700

    Person type theming

--- sites/all/themes/mukurtu/template.php
+++ sites/all/themes/mukurtu/template.php
@@ -31,3 +31,26 @@ function mukurtu_preprocess_panels_pane(&$variables) {
         }
     }
 }
+
+function mukurtu_preprocess_field(&$variables, $hook) {
+    $element = $variables['element'];
+
+    // Add anchors for Person type full view
+    if(!empty($element['#object']->type) && $element['#object']->type == 'person' && $element['#view_mode'] == 'full') {
+
+        // Related People
+        if($element['#field_name'] == 'field_related_people') {
+            $variables['items'][key($variables['items'])]['#prefix'] = '<a name="related-people"></a>';
+        }
+
+        // Sections
+        $section_number = 1;
+        if($element['#field_name'] == 'field_sections') {
+            foreach($element['#items'] as $index => $section) {
+                foreach($variables['items'][$index]['entity']['paragraphs_item'] as $p_index => $paragraph) {
+                    $variables['items'][$index]['entity']['paragraphs_item'][$p_index]['#prefix'] = '<a name="section-' . $section_number++ . '"></a>';
+                }
+            }
+        }
+    }
+}
