49a45f83c1b83b2b31c3fb02eca4e7b50eff05b3
Steve Taylor <steve_taylor@wsu.edu>
2019-05-08 12:58:54 -0700

    Alter Mukurtu theme to make sub-theming easier

--- sites/all/themes/mukurtu/template.php
+++ sites/all/themes/mukurtu/template.php
@@ -5,11 +5,19 @@
  * The primary PHP file for this theme.
  */
 function mukurtu_preprocess_html(&$variables) {
-    $color_scheme = theme_get_setting('mukurtu_theme_color_scheme');
-    $css = join('/', array(path_to_theme(), 'css', "style-{$color_scheme}.css"));
-    if (file_exists($css)) {
-        drupal_add_css($css, array('group' => CSS_THEME, 'every_page' => TRUE));
-    }
+  $color_scheme = theme_get_setting('mukurtu_theme_color_scheme');
+
+  // This option was added for users who wanted to sub-theme with completely recompiled less
+  // files.
+  if ($color_scheme == 'none') {
+    return;
+  }
+
+  // Add the css for the particular color scheme.
+  $css = join('/', array(path_to_theme(), 'css', "style-{$color_scheme}.css"));
+  if (file_exists($css)) {
+    drupal_add_css($css, array('group' => CSS_THEME, 'every_page' => TRUE));
+  }
 }
 
 /**
@@ -84,7 +92,7 @@ function mukurtu_preprocess_field(&$variables, $hook) {
 function mukurtu_block_view_alter(&$data, $block) {
     // Rather than mess with block display, we configure all pre-packaged frontpage
     // blocks to display and hide the ones not selected in the theme settings.
-    $frontpage_hero_setting = theme_get_setting('mukurtu_theme_frontpage_layout', 'mukurtu');
+    $frontpage_hero_setting = theme_get_setting('mukurtu_theme_frontpage_layout');
 
     if(isset($block->bid)) {
         switch($block->bid) {
