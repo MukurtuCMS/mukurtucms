b4081ca93a33f6b19e866ad40851cc17c8c1918b
Shiraz Dindar <shiraz@kanopistudios.com>
2019-06-18 12:57:05 -0700

    Most of the work for having a separate Collections and Personal Collections field from Related Content (undoing what was done earlier, but retaining multiple content types can be in Collection or Personal Collection).

--- sites/all/themes/mukurtu/js/collection-grid.js
+++ sites/all/themes/mukurtu/js/collection-grid.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
     // Grid Handling for collections
     $(window).load(function() {
-	$('.field-name-field-related-content').masonry({
+	$('.field-name-field-digital-heritage-items').masonry({
 	    itemSelector: '.view-mode-search_result',
 	    gutter: 30,
 	});
