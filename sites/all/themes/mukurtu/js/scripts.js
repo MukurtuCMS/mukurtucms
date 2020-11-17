b6381e578cdd3f9f74ec3a204d624ae91a0dac92
Steve Taylor <steve_taylor@wsu.edu>
2020-11-17 14:42:40 -0800

    Add report generator for field bug

--- sites/all/themes/mukurtu/js/scripts.js
+++ sites/all/themes/mukurtu/js/scripts.js
@@ -53,7 +53,11 @@ jQuery(document).ready(function($){
         jQuery(this).parent().attr('data-toggle', 'ckeditor-tooltip');
         jQuery(this).parent().attr('title', jQuery(this).attr('data-original-title'));
     });
-    $('[data-toggle="ckeditor-tooltip"]').tooltip(options);
+    try {
+        $('[data-toggle="ckeditor-tooltip"]').tooltip(options);
+    } catch (err) {
+
+    }
 
     // Hide the add new comment block title if the user can't see the form.
     if ($('#comments form.comment-form').length == 0) {
